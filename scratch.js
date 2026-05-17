538874b2763611857b8b6c7330dc796571fe51c4
VanshS-21 <Vs.personal.id@gmail.com>
2026-05-17 12:37:55 +0530

    chore: remove scratch.js

--- scratch.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const report = JSON.parse(fs.readFileSync('C:/Users/SUPER/AppData/Local/Temp/chrome-devtools-mcp-JcvZOW/report.json', 'utf8'));
-const contrast = report.audits['color-contrast'];
-if(contrast && contrast.details && contrast.details.items) {
-    console.log("Contrast issues:");
-    contrast.details.items.forEach(i => console.log(i.node.snippet));
-}
